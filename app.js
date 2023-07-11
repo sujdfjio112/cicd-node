c7117ab93c8ebd4090352395fef6f3ede869331b
weberX <35339213+sujdfjio112@users.noreply.github.com>
2023-07-11 14:13:55 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const WebSocket = require('ws');
 
 const app = express();
-const port = 80;
+const port = 4000;
 const server = app.listen(port);
 
 // 创建 WebSocket 服务器
