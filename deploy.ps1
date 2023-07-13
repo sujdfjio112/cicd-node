f27db7b7326b87175f44b98a1e131b3f91d1de92
Gavin Xu <pengfeixu@hengtiansoft.com>
2023-07-13 09:57:30 +0800

    123

--- deploy.ps1
+++ deploy.ps1
@@ -1,11 +1,13 @@
 # 测试连接
-Test-NetConnection 115.159.102.152
+ping 115.159.102.152
 
-# 连接到远程服务器
-$session = New-PSSession -ComputerName $env:SERVER_IP -Credential (Get-Credential)
+# 连接到远程服务器，并添加目标计算机到TrustedHosts配置设置中
+$session = New-PSSession -ComputerName $env:SERVER_IP -Credential (Get-Credential) -SessionOption (New-PSSessionOption -SkipCACheck -SkipCNCheck -SkipRevocationCheck)
 
-# 添加
-Set-Item WSMan:\localhost\Client\TrustedHosts -Value $env:SERVER_IP -Force
+# 添加目标计算机到TrustedHosts配置设置中
+Invoke-Command -Session $session -ScriptBlock {
+    Set-Item WSMan:\localhost\Client\TrustedHosts -Value $env:SERVER_IP -Force
+}
 
 # 切换到应用程序目录
 Invoke-Command -Session $session -ScriptBlock {
