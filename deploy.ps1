6c8135f1622a4a30694e73529bb800033b139349
Gavin Xu <pengfeixu@hengtiansoft.com>
2023-07-13 11:29:23 +0800

    777

--- deploy.ps1
+++ deploy.ps1
@@ -2,24 +2,21 @@ $env:SERVER_IP = "115.159.102.152"
 # 测试连接
 Test-NetConnection -ComputerName 115.159.102.152 -Port 3389
 
-Set-Item WSMan:\localhost\Client\TrustedHosts -Value "115.159.102.152" -Concatenate -Force
 
-# 指定远程服务器的计算机名或 IP 地址
-$ComputerName = $env:SERVER_IP
+Write-Host "SERVER_IP = $env:SERVER_IP"
+Write-Host "USERNAME = $env:USERNAME"
+Write-Host "PASSWORD = $env:PASSWORD"
+Write-Host "USERNAME = $env:SERVER_USERNAME"
+Write-Host "PASSWORD = $env:SERVER_PASSWORD"
 
-# 指定远程服务器的用户名和密码
-$Username = $env.USERNAME
-$Password = ConvertTo-SecureString $env.PASSWORD -AsPlainText -Force
-$Credential = New-Object System.Management.Automation.PSCredential($Username, $Password)
+# # 指定要创建的文件路径和内容
+# $FilePath = "C:\temp\test.txt"
+# $FileContent = "Hello, World!"
 
-# 指定要创建的文件路径和内容
-$FilePath = "C:\temp\test.txt"
-$FileContent = "Hello, World!"
-
-# 在远程服务器上创建文件并设置内容
-Invoke-Command -ComputerName $ComputerName -Credential $Credential -ScriptBlock {
-    Set-Content -Path $using:FilePath -Value $using:FileContent
-}
+# # 在远程服务器上创建文件并设置内容
+# Invoke-Command -ComputerName $ComputerName -Credential $Credential -ScriptBlock {
+#     Set-Content -Path $using:FilePath -Value $using:FileContent
+# }
 
 # # 切换到应用程序目录
 # Invoke-Command -Session $session -ScriptBlock {
