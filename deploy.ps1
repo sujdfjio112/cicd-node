5548a6e48f4fe5da72fddf3694532a07404ed34d
Gavin Xu <pengfeixu@hengtiansoft.com>
2023-07-13 11:20:51 +0800

    22

--- deploy.ps1
+++ deploy.ps1
@@ -4,33 +4,42 @@ Test-NetConnection -ComputerName 115.159.102.152 -Port 3389
 
 Set-Item WSMan:\localhost\Client\TrustedHosts -Value "115.159.102.152" -Concatenate -Force
 
-# 连接到远程服务器，并添加目标计算机到TrustedHosts配置设置中
-$session = New-PSSession -ComputerName $env:SERVER_IP -Credential (Get-Credential) -Authentication CredSSP
+# 指定远程服务器的计算机名或 IP 地址
+$ComputerName = $env:SERVER_IP
 
-# 添加目标计算机到TrustedHosts配置设置中
-Invoke-Command -Session $session -ScriptBlock {
-    Set-Item WSMan:\localhost\Client\TrustedHosts -Value $env:SERVER_IP -Force
+# 指定远程服务器的用户名和密码
+$Username = $env.USERNAME
+$Password = ConvertTo-SecureString $env.PASSWORD -AsPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential($Username, $Password)
+
+# 指定要创建的文件路径和内容
+$FilePath = "C:\temp\test.txt"
+$FileContent = "Hello, World!"
+
+# 在远程服务器上创建文件并设置内容
+Invoke-Command -ComputerName $ComputerName -Credential $Credential -ScriptBlock {
+    Set-Content -Path $using:FilePath -Value $using:FileContent
 }
 
-# 切换到应用程序目录
-Invoke-Command -Session $session -ScriptBlock {
-    Set-Location C:\inetpub\wwwroot\myapp
-}
+# # 切换到应用程序目录
+# Invoke-Command -Session $session -ScriptBlock {
+#     Set-Location C:\inetpub\wwwroot\myapp
+# }
 
-# 从 Git 仓库中拉取最新代码
-Invoke-Command -Session $session -ScriptBlock {
-    & git pull origin master
-}
+# # 从 Git 仓库中拉取最新代码
+# Invoke-Command -Session $session -ScriptBlock {
+#     & git pull origin master
+# }
 
-# 安装必要的依赖项
-Invoke-Command -Session $session -ScriptBlock {
-    # Install dependencies
-}
+# # 安装必要的依赖项
+# Invoke-Command -Session $session -ScriptBlock {
+#     # Install dependencies
+# }
 
-# 启动应用程序
-Invoke-Command -Session $session -ScriptBlock {
-    # Start application
-}
+# # 启动应用程序
+# Invoke-Command -Session $session -ScriptBlock {
+#     # Start application
+# }
 
-# 关闭远程会话
-Remove-PSSession $session
+# # 关闭远程会话
+# Remove-PSSession $session
