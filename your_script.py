666a355eb7cde2a34f4db2c8e3ad9450606de9b0
Gavin Xu <pengfeixu@hengtiansoft.com>
2023-08-08 12:30:21 +0800

    3131

--- your_script.py
+++ your_script.py
@@ -1,6 +1,5 @@
 import winrm
 import os
-import subprocess
 
 # 从环境变量中获取 WinRM 连接信息
 host = os.environ["WINRM_HOST"]
@@ -8,43 +7,22 @@ username = os.environ["WINRM_USERNAME"]
 password = os.environ["WINRM_PASSWORD"]
 
 # 创建 WinRM 会话对象
-$session = winrm.Session(host, auth=(username, password))
-
-# # 在远程服务器上创建一个新的文本文件
-# filename = "test.txt"
-# content = "Hello, World!"
-# command = f"New-Item -ItemType File -Path .\\{filename}; Set-Content -Path .\\{filename} -Value '{content}'"
-# result = session.run_ps(command)
-
-# # 打印命令执行结果
-# print(result.std_out)
-# print(result.std_err)
-
-# # 在远程服务器上拉取最新的代码
-# command = 'cd /d "C:\\Users\\Administrator\\Desktop\\ccc\\cicd-node" && git pull'
-# result = session.run_cmd("powershell", ["-Command", command])
-
-# # 打印命令执行结果
-# print(result.std_out)
-# print(result.std_err)
-
+session = winrm.Session(host, auth=(username, password))
 
 # 要跳转到的文件路径
-$path = r'C:\Users\Administrator\Desktop\ccc\cicd-node'
-
+path = r'C:\Users\Administrator\Desktop\ccc\cicd-node'
 
 # 构建 PowerShell 命令
-$powershell_command = "cd '$path'; git pull"
+powershell_command = f"cd '{path}'; git pull"
 
-# 在远程 PowerShell 会话中执行命令
-Invoke-Command -Session $session -ScriptBlock { param($command) Invoke-Expression $command } -ArgumentList $powershell_command
+# 在远程服务器上执行 PowerShell 命令
+result = session.run_cmd("powershell.exe", ["-Command", powershell_command])
 
 # 检查命令执行结果
 if result.status_code == 0:
     print('Yes!')
 else:
     print('No!')
-    
-# 关闭远程 PowerShell 会话
-Remove-PSSession $session
 
+# 关闭远程会话
+session.close()
